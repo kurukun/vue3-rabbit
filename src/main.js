5e8f9edf8c02f338ee4e658a4fefcee5700f586b
梁早清 <xt_lzq@126.com>
2023-06-26 21:38:36 +0800

    完成了home页面

--- src/main.js
+++ src/main.js
@@ -1,20 +1,19 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
 import App from './App.vue'
 import router from './router'
 
 // 引入初始化样式文件
 import '@/styles/common.scss'
 
-// 测试接口函数
-// import {getCategory } from '@/apis/testApi'
-// getCategory().then(res => {
-//     console.log(res);
-// })
+// 引入懒加载指令插件并注册
+import {lazyPlugin} from '@/directives'
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(lazyPlugin)
 
 app.mount('#app')
+
